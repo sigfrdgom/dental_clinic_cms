71ff7d5cbfc72f5763dde95e8e78e239ad89f729
Kevin Martinez <kkevinmartinez7@gmail.com>
2019-09-20 16:50:28 -0600

    Empezando a subir imagenes

--- application/views/services/create.php
+++ application/views/services/create.php
@@ -2,14 +2,18 @@
     
 <div class="row">
     <div class="col-6">
+    <form action="savePictures" method="post" enctype="multipart/form-data" class="p-3" style="background: #dfdfdf">
+
         <img src="" class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt="">
         <div class="form-group">
-          <label for=""></label>
-          <input type="file" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+          <label for="upload"></label>
+          <input type="file" class="form-control" name="upload" id="" aria-describedby="helpId" placeholder="">
         </div>
+        <button type="submit" class="btn btn-primary">Guardar</button>
+    </form>
     </div>
     <div class="col-6">
-    <form action="" method="post" class="p-3" style="background: #dfdfdf">
+    <form action="" method="post" enctype="multipart/form-data" class="p-3" style="background: #dfdfdf">
 
         <div class="form-group">
           <label for="titulo">titulo</label>
