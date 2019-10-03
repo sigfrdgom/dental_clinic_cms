89f1ed91162562547d202b1a6c607b4b0a2f77cd
sigfrdgom <sigfridgom@gmail.com>
2019-10-03 11:18:34 -0600

    Ordenamiento de codigo

--- assets/js/usuario.js
+++ assets/js/usuario.js
@@ -91,7 +91,8 @@ document.getElementById('guardarUsuario').addEventListener('click', function(e){
 					fetch(controlador, {
 						method: metodo,
 						body: datas
-					}) .then(data =>{
+					}) 
+					.then(data =>{
 						//   console.log(data);
 						if(data=="error"){
 							respuesta.innerHTML=
@@ -109,15 +110,6 @@ document.getElementById('guardarUsuario').addEventListener('click', function(e){
 				document.getElementById('mensaje').innerHTML="Parece que el nombre de usuario ya esta en uso"
 			}
 		})
-
-	
-
-	
-	
-
-
-	
-
 });
 
 
@@ -136,15 +128,15 @@ function eliminar() {
 		if (result.value) {
 			fetch('eliminarUsuario/'+this.value, {
 				method: 'DELETE'
-				})
-				.then(() =>{
-					Swal.fire(
-						'Eliminado',
-						'!El usuario ha sido eliminado',
-						'success'
-						)
-					recargar();		
-				})
+			})
+			.then(() =>{
+				Swal.fire(
+					'Eliminado',
+					'!El usuario ha sido eliminado',
+					'success'
+					)
+				recargar();		
+			})
 		}
 	})
 }
@@ -156,11 +148,9 @@ function asignarEventos(){
 	var btnEliminar=document.getElementsByClassName('btnEliminar');
 	
 	for (var index = 0; index <btnEditar.length; index++) {
-	
 		//PARA EMPEZAR A CARGAR
         btnEditar[index].addEventListener('click', accion);
         btnEliminar[index].addEventListener('click', eliminar);
-        
     }
 }
 
@@ -236,50 +226,47 @@ function limpiar(){
 
 document.getElementById("busqueda").addEventListener("keyup", function(){
 	
-	
-	
 	var busqueda=document.getElementById("busqueda").value;
 	if (busqueda!==""&&busqueda!==" ") {
 		var datas= new FormData();
 		datas.append("busqueda", busqueda)
+		
 		fetch('findByCriteria', {
         method: "POST",
         body: datas
-    }).then(res => res.json()).then(datos => {
-				// console.log(datos)
-				var texto="";
-				datos.forEach(element => {
-					
-					if (element.estado==1) {
-						estado="Activo";
-					}else{
-						estado="Desactivado";
-					}
-				 texto+=`
-				<tr class="p-0 border-bottom border-info" id="tr${element.id_usuario}">
-    				<td>${element.nombres}</td>
-    				<td>${element.apellidos}</td>
-    				<td>${element.nombre_usuario}</td>
-					<td>${element.tipo_usuario}</td>
-					<td>${estado}</td>
-            		<td class="py-2">
-						<button class="btnEditar text-center btn btn-warning btn-rounded"  value="${element.id_usuario}" data-toggle="modal" data-target="#agregarUsuario">EDITAR</button>
-						<button class="btnEliminar text-center btn btn-danger btn-rounded"  value="${element.id_usuario}">ELIMINAR</button>
-            		</td>
-    			</tr>`
-				});
-					if (datos.length>0) {
-						respuesta.innerHTML=texto;
-						asignarEventos();	
-					}else{
-						respuesta.innerHTML="NO HAY REGISTRO COINCIDENTES";
-					}
-					
-			})
+		})
+		.then(res => res.json()).then(datos => {
+			// console.log(datos)
+			var texto="";
+			datos.forEach(element => {
+				
+				if (element.estado==1) {
+					estado="Activo";
+				}else{
+					estado="Desactivado";
+				}
+				texto+=`
+					<tr class="p-0 border-bottom border-info" id="tr${element.id_usuario}">
+						<td>${element.nombres}</td>
+						<td>${element.apellidos}</td>
+						<td>${element.nombre_usuario}</td>
+						<td>${element.tipo_usuario}</td>
+						<td>${estado}</td>
+						<td class="py-2">
+							<button class="btnEditar text-center btn btn-warning btn-rounded"  value="${element.id_usuario}" data-toggle="modal" data-target="#agregarUsuario">EDITAR</button>
+							<button class="btnEliminar text-center btn btn-danger btn-rounded"  value="${element.id_usuario}">ELIMINAR</button>
+						</td>
+					</tr>`
+					});
+				if (datos.length>0) {
+					respuesta.innerHTML=texto;
+					asignarEventos();	
+				}else{
+					respuesta.innerHTML="NO HAY REGISTRO COINCIDENTES";
+				}
+				
+		})
 		
-
-
-
 	}else{
 		document.getElementById("busqueda").value="";
 		recargar();
@@ -288,8 +275,7 @@ document.getElementById("busqueda").addEventListener("keyup", function(){
 });
 
 
-
-
+/////////--------------------------------- Para mostrar automaticament el usuario recomendado ----------------------------///////////
 document.getElementById('nombres').addEventListener('input', genUsuario)
 document.getElementById('apellidos').addEventListener('input', genUsuario)
 
@@ -301,21 +287,3 @@ function genUsuario() {
 	document.getElementById('usuario').value= usuario
 }
 
-function valUsuario() {
-	usuario = document.getElementById('usuario').value 
-	fetch('validarUsuario/'+usuario)
-		.then(res => res.json())
-		.then(datos => {
-			if (datos == null) {
-				if (document.getElementById('nombres').value === "" && document.getElementById('apellidos').value === "") {
-					document.getElementById('usuario').value = ""
-				} else {
-					document.getElementById('usuario').value = usuario
-				}
-				console.log("valido") 
-			}else{
-				console.log("invalido")
-				
-			}
-		})
-}
