d7cc2f5b674864427a8b19860b8cc7d0be27f6af
Matl21 <carlos.animalia@gmail.com>
2019-10-09 15:06:28 -0600

    Cambios menores contacto y publicacion

--- application/models/publicacion_model.php
+++ application/models/publicacion_model.php
@@ -79,12 +79,17 @@ class Publicacion_model extends CI_Model
             $this->db->order_by('fecha_ingreso', 'DESC');
             return $this->db->get('publicacion')->result();
         } else {
-            $this->db->like('titulo', $keyword);
-            $this->db->or_like('texto_introduccion', $keyword);
-            $this->db->or_like('contenido', $keyword);
-            $this->db->where('id_tipo', 2);
-            $this->db->order_by('fecha_ingreso', 'DESC');
-            return $this->db->get('publicacion')->result();
+			$escape="'%".$keyword."%' ESCAPE '!'";
+			$query=$this->db->query('SELECT * FROM publicacion WHERE `id_tipo` = 2 AND ( `titulo` LIKE '.$escape.' OR `texto_introduccion` LIKE '.$escape.' OR `contenido` LIKE '.$escape.') ORDER BY `fecha_ingreso` DESC');
+
+			return $query->result();
+
+			// $this->db->where('id_tipo', 2);		
+            // $this->db->like('titulo', $keyword);
+            // $this->db->or_like('texto_introduccion', $keyword);
+			// $this->db->or_like('contenido', $keyword);
+            // $this->db->order_by('fecha_ingreso', 'DESC');
+            // return $this->db->get('publicacion')->result();
         }
     }
 }
