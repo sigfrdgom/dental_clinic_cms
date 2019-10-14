4267e1c9261a41acf16638b6b0859521ac5467c7
sigfrdgom <sigfridgom@gmail.com>
2019-10-14 13:32:48 -0600

    pequeños cambios

--- application/config/config.php
+++ application/config/config.php
@@ -23,6 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
+// $config['base_url'] = 'http://localhost/dental_clinic_cms/';
+
 $config['base_url'] = 'http://localhost/dental_clinic_cms/';
 
 /*
